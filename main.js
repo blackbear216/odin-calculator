6563c74e19b707f2ab2b8aff9f2505abe987b77a
blackbear <98885273+blackbear216@users.noreply.github.com>
2023-08-27 11:40:16 +0100

    Basic arithmetic functions

---
+++ main.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(5, 7));
+console.log(subtract(5, 7));
+console.log(multiply(5, 7));
+console.log(divide(5, 7));
+console.log(divide(5, 0));
